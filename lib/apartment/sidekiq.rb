3931aec8644474d34def77a9513b1dd7a029e52d
Brad Robertson and Zulfiqar Ali <dev+brad+zulfiqar@influitive.com>
2013-07-19 16:35:18 -0400

    move middleware delcaration out of Railtie for Rack apps

--- lib/apartment/sidekiq.rb
+++ lib/apartment/sidekiq.rb
@@ -1,10 +1,29 @@
-require "apartment/sidekiq/version"
+require 'apartment/sidekiq/version'
+require 'sidekiq'
 
 module Apartment
   module Sidekiq
     module Middleware
       autoload :Client, 'apartment/sidekiq/middleware/client'
       autoload :Server, 'apartment/sidekiq/middleware/server'
+
+      def self.run
+        ::Sidekiq.configure_client do |config|
+          config.client_middleware do |chain|
+            chain.add Apartment::Sidekiq::Middleware::Client
+          end
+        end
+
+        ::Sidekiq.configure_server do |config|
+          config.client_middleware do |chain|
+            chain.add Apartment::Sidekiq::Middleware::Client
+          end
+
+          config.server_middleware do |chain|
+            chain.add Apartment::Sidekiq::Middleware::Server
+          end
+        end
+      end
     end
   end
 end
--- lib/apartment/sidekiq/railtie.rb
+++ lib/apartment/sidekiq/railtie.rb
@@ -1,23 +1,7 @@
 module Apartment::Sidekiq
   class Railtie < Rails::Railtie
     initializer "apartment.sidekiq" do
-      require 'sidekiq'
-
-      Sidekiq.configure_client do |config|
-        config.client_middleware do |chain|
-          chain.add Apartment::Sidekiq::Middleware::Client
-        end
-      end
-
-      Sidekiq.configure_server do |config|
-        config.client_middleware do |chain|
-          chain.add Apartment::Sidekiq::Middleware::Client
-        end
-
-        config.server_middleware do |chain|
-          chain.add Apartment::Sidekiq::Middleware::Server
-        end
-      end
+      Apartment::Sidekiq::Middleware.run
     end
   end
 end
